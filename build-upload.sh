30b465ae81492b883fb34e1f6d157abdf3db67b2
wangshanhai <ilikeshatang@gmail.com>
2018-09-05 15:34:43 +0800

    svga动画文件

--- build-upload.sh
+++ build-upload.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-echo "start build ssembleRelease------------------------------"
-gradle clean
-gradle  assembleRelease
+echo "start  bintrayUpload------------------------------"
+gradlew clean build bintrayUpload -PbintrayUser=ilike -PbintrayKey=d6d13496e61fc40a68bf110103226e74821580d9  -PdryRun=true
